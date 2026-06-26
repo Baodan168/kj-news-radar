71fa08235d2099a19e3bdad30495e57d93afa55b
liyuhong168 <liyuhong168@users.noreply.github.com>
2026-06-26 15:31:57 +0800

    hotfix: 移除renderUrgencyFilter调用

--- assets/app.js
+++ assets/app.js
@@ -1023,7 +1023,6 @@ function renderAll() {
   renderModeSwitch();
   renderSiteFilters();
   renderImpactFilter();
-  renderUrgencyFilter();
   renderPlatformFilter();
   renderList();
   renderCrossPicks();
